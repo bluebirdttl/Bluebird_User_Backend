5311f9825cab0762b6ce3d120cc9ff40322a36b7
AbhinavPandey12 <abhinavpandey101@gmail.com>
2025-12-15 11:43:06 +0530

    Versal Push

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 // -----------------------------
 app.use(cors({
     origin: [
-        'http://localhost:3000',
-        'http://localhost:5000'
+        'https://bluebird-star-app.vercel.app',
+        'https://bluebird-user-backend.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     credentials: true
